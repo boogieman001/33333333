b322a904b8b948f3bb4dc2d1705ee736e36438a0
boogieman001 <113910819+boogieman001@users.noreply.github.com>
2022-11-16 09:44:20 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    list = [
+    1,
+    2,
+    3,
+    4,
+    5
+    ]
+})
+let list: number[] = []
+basic.showIcon(IconNames.Yes)
+basic.forever(function () {
+	
+})
